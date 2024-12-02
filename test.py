e36541056491d472fb602219b220e89ae74afb13
Manuel Körber <manuel.koerber@eyeworkers.de>
2024-12-02 10:22:51 +0100

    feat: impement distance between pointers in two lists

--- test.py
+++ test.py
@@ -1,11 +1,35 @@
-list1 = []
-list2 = []
+from typing import List
+
+
+list1: List[int] = []
+list2: List[int] = []
 
 with open("input.txt", 'r') as file:
     lines = file.readlines()
     for idx, line in enumerate(lines):
         line = line.replace("\n", "")
-        if idx % 2:
-            list1.append(line)
-        else:
-            list2.append(line)
+        lineList = line.split("   ")
+        for idx, lineNum in enumerate(lineList):
+            if idx == 0:
+                list1.append(int(lineNum))
+            else:
+                list2.append(int(lineNum))
+
+list1.sort()
+list2.sort()
+
+distances: List[int] = []
+
+for idx, pointer1 in enumerate(list1):
+    pointer2 = list2[idx]
+    if pointer1 > pointer2:
+        distances.append(pointer1 - pointer2)
+    else:
+        distances.append(pointer2 - pointer1)
+
+sum: int = 0
+
+for distance in distances:
+    sum += distance
+
+print(sum)
